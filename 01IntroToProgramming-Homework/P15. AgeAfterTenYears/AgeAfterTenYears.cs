aedae2edb43d3219e481a1f9276761d800e3c256
irgeorgieva <irgeorgieva@gmail.com>
2014-10-21 18:33:03 +0300

    01IntroToProgramming-Homework
    01IntroToProgramming-Homework

---
+++ 01IntroToProgramming-Homework/P15. AgeAfterTenYears/AgeAfterTenYears.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class AgeAfterTenYears
+{
+    static void Main()
+    {
+        Console.WriteLine("Please enter your birthday");
+        DateTime birthDay = DateTime.Parse(Console.ReadLine());
+        int years = DateTime.Now.Year - birthDay.Year;
+        Console.WriteLine("You are {0} years old.", years);
+        int ageInTen = years + 10;
+        Console.WriteLine("In 10 years, you are going to be {0} years old.", ageInTen);
+
+
+    }
+}
